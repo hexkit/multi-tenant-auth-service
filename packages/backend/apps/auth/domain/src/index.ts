7269f44d33e09801671c134c038bc5dd08e60e77
notaphplover <roberto.pintos.lopez@gmail.com>
2026-01-28 13:54:17 +0100

    chore: add auth domain package

---
+++ packages/backend/apps/auth/domain/src/index.ts
@@ -0,0 +1,4 @@
+export { type User } from './user/domain/models/User';
+export { type UserFindQuery } from './user/domain/models/UserFindQuery';
+export { type UserSetQuery } from './user/domain/models/UserSetQuery';
+export { type UserUpdateQuery } from './user/domain/models/UserUpdateQuery';
